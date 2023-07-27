5147ed313e8e30cd30911055e0e7647e4093b333
ZackSanchez227 <107153115+ZackSanchez227@users.noreply.github.com>
2023-07-27 03:22:09 -0700

    Add files via upload

--- javascript-es5/Organization.js
+++ javascript-es5/Organization.js
@@ -11,10 +11,66 @@ module.exports = function (root) {
     },
     // Hire the given person as an employee in the position that has that title
     // Return the newly filled position or undefined if no position has that title
-    hire: function (person, title) {
+    hire: function (person, title)
+    {
       // your code here
+
+      function findPosition(position, targetTitle) // Find position given title.
+      {
+        if (position.getTitle() === targetTitle) {
+          return position;
+        }
+
+        for (const p of position.getDirectReports())
+        {
+          const foundPosition = findPosition(p, targetTitle);
+          if (foundPosition)
+          {
+            return foundPosition;
+          }
+        }
+
+        return undefined;
+      }
+
+      const targetPosition = findPosition(root, title);
+
+      if (targetPosition && !targetPosition.isFilled())
+      {
+        targetPosition.setEmployee(person);
+        return targetPosition;
+      }
+
+      return undefined;
+      },
+/*
+Alternative solution iterative DFS
+hire: function (person, title)
+{
+      const stack = [root];     Uses stack to perform iterative DFS traversal
+
+      while (stack.length > 0)
+       {
+        const current = stack.pop();
+
+        if (current.getTitle() === title && !current.isFilled())
+         {
+          current.setEmployee(person);
+          return current;
+        }
+
+        for (const p of current.getDirectReports()) Push all direct reports of current position onto the stack for traversal.
+        {
+          stack.push(p);
+        }
+      }
+
+      // If no position with the given title is found or it's already filled, return undefined
+      return undefined;
     },
 
+
+ */
     toString: function () {
       return this.printOrganization(root, '');
     }
