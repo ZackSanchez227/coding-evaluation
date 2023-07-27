15c4e6106ee3b440696795ca319029a4bc156bbf
ZackSanchez227 <107153115+ZackSanchez227@users.noreply.github.com>
2023-07-27 03:22:27 -0700

    Add files via upload

--- javascript-es6/Organization.js
+++ javascript-es6/Organization.js
@@ -1,3 +1,5 @@
+import Employee from "./Employee.js";
+
 class Organization {
   constructor(root) {
     this.printOrganization = (position, prefix) => {
@@ -10,8 +12,29 @@ class Organization {
 
     // Hire the given person as an employee in the position that has that title
     // Return the newly filled position or undefined if no position has that title
-    this.hire = (person, title) => {
+    this.hire = (person, title) =>
+    {
       // your code here
+      const queue = [root];
+
+      while (queue.length > 0)
+      {
+        const current = queue.shift();
+
+        if (current.getTitle() === title && !current.isFilled()) //Check current position and if not filled.
+        {
+          current.setEmployee(new Employee(Date.now(), person)); //Fill position as an employee.
+          return current; //return filled position
+        }
+
+        const directReports = current.getDirectReports();
+        if (directReports.length > 0) {
+          queue.push(...directReports);
+        }
+      }
+
+      // If the position is not found or already filled, return undefined
+      return undefined;
     };
 
     this.toString = () => this.printOrganization(root, '');
